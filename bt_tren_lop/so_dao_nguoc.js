a63d01930aa62a433ba5decb2c11e55b0d3b81a2
HuyNguyenDucBao <149989165+HuyNguyenDucBao@users.noreply.github.com>
2024-02-26 09:07:58 +0700

    main

---
+++ bt_tren_lop/so_dao_nguoc.js
@@ -0,0 +1,13 @@
+let n = prompt("Nhập vào một số nguyên dương n");
+n = parseInt(n);
+
+if (isNaN(n) || n <= 0) {
+    console.log("Vui lòng nhập một số nguyên dương.");
+} else {
+    let reversedNumber = 0;
+    while (n > 0) {
+        reversedNumber = reversedNumber * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    console.log(`Số đảo ngược là ${reversedNumber}`);
+}
