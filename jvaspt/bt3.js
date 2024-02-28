c4011b78d1f11150d6165c64a2afc5d6f0a3b060
HuyNguyenDucBao <149989165+HuyNguyenDucBao@users.noreply.github.com>
2024-02-28 21:59:40 +0700

    main

---
+++ jvaspt/bt3.js
@@ -0,0 +1,16 @@
+const inputStr = prompt("Nhập vào chuỗi bất kỳ: ");
+
+const allSubstrings = [];
+
+for (let i = 0; i < inputStr.length; i++) {
+  let substring = '';
+  for (let j = i; j < inputStr.length; j++) {
+    substring += inputStr[j];
+    allSubstrings.push(substring);
+  }
+}
+
+console.log("Danh sách tất cả các chuỗi con:");
+for (const substring of allSubstrings) {
+  console.log(substring);
+}
