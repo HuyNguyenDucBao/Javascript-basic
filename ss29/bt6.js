ec0298696df97dbc374682242efda8d0c1dfffd5
HuyNguyenDucBao <149989165+HuyNguyenDucBao@users.noreply.github.com>
2024-03-04 08:20:36 +0700

    main

--- ss29/bt6.js
+++ ss29/bt6.js
@@ -1,10 +1,9 @@
 var chuoi = prompt("Nhập một chuỗi kí tự bất kì: ");
 
 function vietHoaChuCaiDau(chuoi) {
-    var arrChuoi = chuoi.split(" ");
-    for (var i = 0; i < arrChuoi.length; i++) {
-        arrChuoi[i] = arrChuoi[i].charAt(0).toUpperCase() + arrChuoi[i].slice(1).toLowerCase();
-    }
-    return arrChuoi.join(" ");
+    return chuoi.split(" ").map(function(tu) {
+        return tu.charAt(0).toUpperCase() + tu.slice(1).toLowerCase();
+    }).join(" ");
 }
+
 console.log(vietHoaChuCaiDau(chuoi));
